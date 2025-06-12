1d879be111dee81a3e4a9e330bf30f4bd331fae2
daniel <deospr@gmail.com>
2025-06-12 12:06:24 +0300

    Added ProtectedRoute.jsx to test un/authorized access

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,6 +1,32 @@
-import { Navigate } from 'react-router-dom';
+import {Navigate, useNavigate} from 'react-router-dom';
+import {useState, useEffect, } from "react";
+import api from "../services/api";
 
 export default function ProtectedRoute ({children}) {
+    const [status, setStatus] = useState('checking');
+  const navigate = useNavigate();
+
+  useEffect(() => {
     const token = localStorage.getItem('token');
-    return token ? children: <Navigate to="/login" replace/>;
+    if (!token) {
+      setStatus('invalid');
+      return;
+    }
+
+    api.get('/auth/validate', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(() => setStatus('valid'))
+    .catch(() => {
+      localStorage.removeItem('token');
+      setStatus('invalid');
+      navigate('/login');
+    });
+  }, [navigate]);
+
+  if (status === 'checking') return <div>Loading...</div>;
+  if (status === 'invalid') return <Navigate to="/login" replace />;
+  return children;
 }
