e550a2897990cb2e2c792e4c49ddb8957ec45c79
daniel <deospr@gmail.com>
2025-06-17 22:37:04 +0300

    Added update, delete user tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -183,3 +183,93 @@ async def test_get_user_by_uid_role_and_ownership(
 
     response = await client.get(f"/api/v1/users/{target_uid}")
     assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    "actor_role,is_self, expected_status",
+    [
+        (UserRole.ADMIN, False, 200),
+        (UserRole.USER, True, 200),
+        (UserRole.USER, False, 403),
+    ],
+)
+@pytest.mark.asyncio
+async def test_update_user_by_uid_role_and_ownership(
+    client,
+    mock_user_factory,
+    override_current_user,
+    actor_role,
+    is_self,
+    expected_status,
+):
+    # создадим пользователя
+    response = await client.post(
+        "/api/v1/users/signup",
+        json={
+            "username": "update_target",
+            "email": "update@target.com",
+            "first_name": "Old",
+            "last_name": "Oldest",
+            "password": "securepassword",
+        },
+    )
+
+    assert response.status_code == 201
+    user_uid = response.json()["result"]["uid"]
+
+    # создадим того, что будет производить действие (обновлять данные)
+    uid = uuid.UUID(user_uid) if is_self else uuid.uuid4()
+    mock_user = mock_user_factory(actor_role, uid=uid)
+    override_current_user(mock_user)
+
+    # обновляем данные
+    response = await client.patch(
+        f"/api/v1/users/{user_uid}",
+        json={"first_name": "New", "last_name": "Newest"},
+    )
+    assert response.status_code == expected_status
+
+    if expected_status == 200:
+        assert response.json()["result"]["first_name"] == "New"
+
+
+@pytest.mark.parametrize(
+    "actor_role, is_self, expected_status",
+    [
+        (UserRole.ADMIN, False, 204),
+        (UserRole.USER, True, 204),
+        (UserRole.USER, False, 403),
+    ],
+)
+@pytest.mark.asyncio
+async def test_delete_user_by_uid_role_and_ownership(
+    client,
+    mock_user_factory,
+    override_current_user,
+    actor_role,
+    is_self,
+    expected_status,
+):
+    response = await client.post(
+        "/api/v1/users/signup",
+        json={
+            "username": "delete_target",
+            "email": "delete@target.com",
+            "first_name": "Delete",
+            "last_name": "User",
+            "password": "securepassword",
+        },
+    )
+    assert response.status_code == 201
+    user_uid = response.json()["result"]["uid"]
+
+    uid = uuid.UUID(user_uid) if is_self else uuid.uuid4()
+    mock_user = mock_user_factory(actor_role, uid=uid)
+    override_current_user(mock_user)
+
+    response = await client.delete(f"/api/v1/users/{user_uid}")
+    assert response.status_code == expected_status
+    if expected_status == 204:
+        override_current_user(mock_user_factory(UserRole.ADMIN))
+        response = await client.get(f"/api/v1/users/{user_uid}")
+        assert response.status_code == 404
