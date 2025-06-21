9fd8352d4b6ae3ada75e659e9342361e8d86650a
daniel <deospr@gmail.com>
2025-06-21 22:28:15 +0300

    Styles for AppLayout.jsx

--- frontend/src/pages/AppLayout.jsx
+++ frontend/src/pages/AppLayout.jsx
@@ -1,5 +1,4 @@
 import {Link, NavLink, Outlet, useNavigate} from "react-router-dom";
-import MyWhipLogo from "../components/MyWhipLogo.jsx";
 import logo from "../assets/logo.png";
 
 export default function AppLayout() {
@@ -10,28 +9,27 @@ export default function AppLayout() {
     };
 
     return (
-        <div>
-            <nav className="flex justify-center items-center bg-blue-800 text-white gap-2">
-                <Link to="/">
-                    <div className='flex items-center justify-center gap-x-2 '>
-                        <img src={logo} alt="my_whip_logo" className="h-10 w-auto object-contain"/>
-                        <div className="flex flex-col">
-                            <p className="text-white font-bold">My</p>
-                            <p className="text-white font-bold">Whip</p>
-                        </div>
+        <div className="min-h-screen">
+            <nav className="flex flex-wrap items-center justify-between px-4 py-2 bg-[#222] text-white">
+                <Link to="/" className="flex items-center gap-2">
+                    <img src={logo} alt="my_whip_logo" className="h-10 w-auto object-contain"/>
+                    <div className="flex flex-col leading-none">
+                        <span className="font-bold">My</span>
+                        <span className="font-bold">Whip</span>
                     </div>
                 </Link>
-                <div>
+
+                <div className="flex sm:flex-row sm:items-center gap-4 px-4">
                     <NavLink to="cars" className={({isActive}) => isActive ? "text-blue-300" : ""}>
-                        Мои автомобили</NavLink>
+                        Мои автомобили
+                    </NavLink>
+                    <NavLink to="profile" className={({isActive}) => isActive ? "text-blue-300" : ""}>
+                        Мой профиль
+                    </NavLink>
+                    <button onClick={logout} className="text-white hover:text-red-300 transition">
+                        Выйти
+                    </button>
                 </div>
-                <div>
-                    <NavLink to="profile" className={({isActive}) => isActive ? "text-blue-300" : ""}>Мой
-                        профиль</NavLink>
-                </div>
-                <button onClick={logout}>
-                    <p className="cursor-pointer">Выйти</p>
-                </button>
             </nav>
             <main>
                 <Outlet/>
--- frontend/src/pages/CarList.jsx
+++ frontend/src/pages/CarList.jsx
@@ -32,10 +32,10 @@ export default function CarsList() {
 
     return (
         <div className='p-4'>
-            <div className='flex justify-between items-center mb-4'>
+            <div className='flex justify-between text-white  mb-4'>
                 <h1 className='text-2xl font-bold'>Мои автомобили</h1>
                 <Link to='new'
-                      className="inline-block mb-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
+                      className="inline-block mb-4 px-4 py-1 bg-green-600  rounded hover:bg-green-700">
                     + Добавить
                 </Link>
             </div>
