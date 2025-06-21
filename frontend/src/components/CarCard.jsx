253661ab076924746bc8dced659898de3aa9372c
daniel <deospr@gmail.com>
2025-06-21 21:21:58 +0300

    Added Creating Car error alerts

--- frontend/src/components/CarCard.jsx
+++ frontend/src/components/CarCard.jsx
@@ -1,13 +1,18 @@
 import {Link} from "react-router-dom";
+import carImg from "../assets/car_poster.jpg";
 
 export default function CarCard({car}) {
+    const image = car.image || carImg;
     return (
         <Link
             to={`/app/cars/${car.uid}`}
-            className="block bg-white rounded-xl shadow hover:shadow-md transition overflow-hidden"
+            className="flex flex-col bg-amber-100 rounded-xl shadow hover:shadow-md transition h-60 overflow-hidden"
         >
-            <img src={car.image} alt={`${car.make} ${car.model}`} className="w-full h-40 object-cover"/>
-            <div className="p-4">
+            <div className='h-40 w-full overflow-hidden'>
+                <img src={image} alt={`${car.make} ${car.model}`} className="w-full h-full object-cover "/>
+            </div>
+            <div className="p-4 flex-1 flex-col justify-between">
+
                 <h2 className="text-xl font-semibold">{car.make} {car.model}</h2>
                 <p className="text-gray-600">Год: {car.year}</p>
                 <p className="text-sm text-blue-600">Статус: {car.status}</p>
--- frontend/src/pages/CarList.jsx
+++ frontend/src/pages/CarList.jsx
@@ -1,26 +1,35 @@
+import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
-import CarCard from "../components/CarCard.jsx"
-import carImg from "../assets/car_poster.jpg"
+import CarCard from "../components/CarCard.jsx";
+import api from "../services/api";
+import carImg from "../assets/car_poster.jpg";
 
 export default function CarsList() {
-    const cars = [
-        {
-            uid: 1,
-            make: "Toyota",
-            model: "Camry",
-            year: 2015,
-            status: "в продаже",
-            image: carImg
-        },
-        {
-            uid: 2,
-            make: "BMW",
-            model: "X5",
-            year: 2018,
-            status: "продан",
-            image: carImg
-        }
-    ];
+    const [cars, setCars] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchCars = async () => {
+            try {
+                const res = await api.get('/cars/my_cars');
+                setCars(res.data.result.content);
+            } catch (err) {
+                console.error("Error while fetching my cars data: ", err);
+                setError("Failed to load my cars");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchCars();
+    }, []);
+    if (loading) {
+        return <div className="p-4">Загружаю карточки автомобилей...</div>;
+    }
+    if (error) {
+        return <div className="p-4 text-red-600">{error}</div>;
+    }
+
     return (
         <div className='p-4'>
             <div className='flex justify-between items-center mb-4'>
@@ -30,11 +39,15 @@ export default function CarsList() {
                     + Добавить автомобиль
                 </Link>
             </div>
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-                {cars.map((car) => (
-                    <CarCard key={car.uid} car={car}/>
-                ))}
-            </div>
+            {cars.length === 0 ? (
+                <p>У вас пока нет машин.</p>
+            ) : (
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+                    {cars.map((car) => (
+                        <CarCard key={car.uid} car={car}/>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
